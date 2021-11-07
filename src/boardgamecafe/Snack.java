e25e20a3854d7da7c34b669691d5968d0a30953d
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-11-07 15:47:42 +0900

    Add files via upload

--- src/boardgamecafe/Snack.java
+++ src/boardgamecafe/Snack.java
@@ -10,29 +10,4 @@ public class Snack implements Manageable {
     // 에스프레소 커피 3800 20
     String name;
     String kind;
-    int price;
-    int quantity;
-
-    @Override
-    public void read(Scanner scan) {
-        name = scan.next();
-        kind = scan.next();
-        price = scan.nextInt();
-        quantity = scan.nextInt();
-    }
-
-    @Override
-    public void print() {
-        System.out.printf("이름: %s 종류: %s 가격: %d 수량: %d\n",
-                name, kind, price, quantity);
-    }
-
-    @Override
-    public boolean matches(String kwd) {
-        if (name.contains(kwd))
-            return true;
-        if (kind.equals(kwd))
-            return true;
-        return false;
-    }
-}
+ 

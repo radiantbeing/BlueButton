ff9974a5faf3785f4d3c0c361463162726cc61e3
INHWA KIM <86849459+radiantbeing99@users.noreply.github.com>
2021-11-10 22:57:38 +0900

    잔여 시간 계산 메소드 추가. 좌석 필드 추가.

--- src/boardgamecafe/NonMember.java
+++ src/boardgamecafe/NonMember.java
@@ -2,9 +2,6 @@ package boardgamecafe;
 
 import mgr.Manageable;
 
-import java.time.Duration;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NonMember implements Manageable {
@@ -15,28 +12,22 @@ public class NonMember implements Manageable {
     int roomNumber;
     int remainingTime;
     Timer timer;
+
     @Override
     public void read(Scanner scan) {
         name = scan.next();
         phoneNumber = scan.next();
-        remainingTime = (int)(Math.random()*255);//remain정해짐 일단 랜덤으로
-        timer = new Timer(remainingTime);
-        timer.start();
     }
 
     @Override
     public void print() {
         printUserType();
         System.out.printf("%s 전화번호: %s\n", name, phoneNumber);
-        timer.printRemainingTime();
+        printRemainingTime();
     }
 
     public void printUserType() {
         System.out.print("[비회원] ");
-    }
-
-    int getRemainingTime(){
-       return remainingTime;
     }
 
     @Override
@@ -48,32 +39,41 @@ public class NonMember implements Manageable {
         return false;
     }
 
-    void setRemainingTime(int remainingTime){
-        this.remainingTime = remainingTime;
-    }
-
     void setRoomNumber(int num) {
         roomNumber = num;
     }
-}
 
-class Timer extends Thread{
-    int remainingTime;
-    public Timer(int remainingTime){
-        this.remainingTime = remainingTime;
+    // 이용 시간을 초 단위로 추가합니다.
+    // GUI의 요금제 선택 단계에서 사용하는 메소드
+    void addTime(int seconds) {
+        remainingTime += seconds;
     }
-    public void printRemainingTime(){
-        System.out.println("잔여시간:"+remainingTime);
+
+    // 잔여 시간을 계산하기 위한 Timer를 시작합니다.
+    // GUI의 결제 단계에서 사용하는 메소드
+    void startTimer() {
+        timer = new Timer();
+        timer.start();
     }
-    public void run() {
-        while(true) {
-            try {
-                sleep(1000);
-                remainingTime--;
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+
+    public void printRemainingTime() {
+        System.out.printf("\t잔여시간: %d\n", remainingTime);
+    }
+
+    class Timer extends Thread {
+        public void run() {
+            while (true) {
+                try {
+                    sleep(1000);
+                    remainingTime--;
+                    if (remainingTime <= 0){
+                        remainingTime = 0;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 }
+

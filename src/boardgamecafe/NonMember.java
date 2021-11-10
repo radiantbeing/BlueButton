9fe9a450a6820a41c2edd15b53301915f55cc618
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-11-10 22:35:07 +0900

    ADD Commit 남은시간추가

--- src/boardgamecafe/NonMember.java
+++ src/boardgamecafe/NonMember.java
@@ -2,30 +2,41 @@ package boardgamecafe;
 
 import mgr.Manageable;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NonMember implements Manageable {
-    // 이름 전화번호
     // 김성철 01055789968
     String name;
     String phoneNumber;
-    ArrayList<Snack> snacksList = new ArrayList<>();//임시 snackList
-
+    // 게임 방은 1, 2, 3, ..., 9, 10의 숫자로 구분
+    int roomNumber;
+    int remainingTime;
+    Timer timer;
     @Override
     public void read(Scanner scan) {
         name = scan.next();
         phoneNumber = scan.next();
+        remainingTime = (int)(Math.random()*255);//remain정해짐 일단 랜덤으로
+        timer = new Timer(remainingTime);
+        timer.start();
     }
 
     @Override
     public void print() {
         printUserType();
         System.out.printf("%s 전화번호: %s\n", name, phoneNumber);
+        timer.printRemainingTime();
     }
 
     public void printUserType() {
         System.out.print("[비회원] ");
+    }
+
+    int getRemainingTime(){
+       return remainingTime;
     }
 
     @Override
@@ -37,7 +48,32 @@ public class NonMember implements Manageable {
         return false;
     }
 
-    public void addSnackList(Snack snack){
-        snacksList.add(snack);
+    void setRemainingTime(int remainingTime){
+        this.remainingTime = remainingTime;
+    }
+
+    void setRoomNumber(int num) {
+        roomNumber = num;
+    }
+}
+
+class Timer extends Thread{
+    int remainingTime;
+    public Timer(int remainingTime){
+        this.remainingTime = remainingTime;
+    }
+    public void printRemainingTime(){
+        System.out.println("잔여시간:"+remainingTime);
+    }
+    public void run() {
+        while(true) {
+            try {
+                sleep(1000);
+                remainingTime--;
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }

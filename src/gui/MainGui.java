bb37f15af6a9db209fe7b6f328808e152aeef0ec
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-11-11 21:25:09 +0900

    ADD MainGUI

--- src/gui/MainGui.java
+++ src/gui/MainGui.java
@@ -1,4 +1,33 @@
 package gui;
 
-public class MainGui {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainGui extends JFrame{
+    JLabel leftSeatsLabel = new JLabel("남은 방:");
+    JPanel seatsIMG = new JPanel();
+    JButton nextButton = new JButton("Log-In");
+
+    public void addComponent(Container container){
+        leftSeatsLabel.setFont(new Font("남은 방",Font.BOLD, 50));
+        leftSeatsLabel.setForeground(Color.blue);
+        leftSeatsLabel.setHorizontalAlignment(JLabel.CENTER);
+        container.add(leftSeatsLabel,BorderLayout.NORTH);
+        container.add(seatsIMG, BorderLayout.CENTER);
+        container.add(nextButton, BorderLayout.SOUTH);
+    }
+
+    public void createGUI() {
+        JFrame jFrame = new JFrame("Swing assignment");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        addComponent(jFrame);
+        jFrame.pack();
+        jFrame.setSize(new Dimension(300, 300));
+        jFrame.setLocation(500,200);
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[]args){
+        new MainGui().createGUI();
+    }
 }

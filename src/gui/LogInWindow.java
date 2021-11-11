6a55fd2ff65195f0867fd6d9bda9f095636d95b3
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-11-12 02:59:54 +0900

    회원, 비회원 GUI수정

--- src/gui/LogInWindow.java
+++ src/gui/LogInWindow.java
@@ -8,13 +8,22 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public class LogInWindow extends Template {
     JPanel memberPanel = new JPanel();
     JPanel nonMemberPanel = new JPanel();
+    JLabel explainLabel = new JLabel();
+    JButton signupButton;
+    JButton signInButton;
+
     @Override
     void addComponentsToPane() {
         primaryPanel.setLayout(null);
         memberLoginWindow();
         nonmemberLoginWindow();
+        textWindow();
+        buttonComponent();
         primaryPanel.add(memberPanel);
         primaryPanel.add(nonMemberPanel);
+        primaryPanel.add(explainLabel);
+        primaryPanel.add(signupButton);
+        primaryPanel.add(signInButton);
     }
 
     void memberLoginWindow(){
@@ -22,37 +31,36 @@ public class LogInWindow extends Template {
         memberPanel.setLayout(null);
         memberPanel.setBackground(Color.white);
         memberPanel.setPreferredSize(new Dimension(500,500));
-        memberPanel.setBounds(100,150,300,400);
+        memberPanel.setBounds(100,90,300,400);
 
         //Member Label
         JLabel memberNameLabel = new JLabel("회원");
 
         memberNameLabel.setForeground(new Color(48, 87, 232));
-        memberNameLabel.setFont(new Font("맑은고딕",Font.BOLD, 30));
-        memberNameLabel.setBounds(40,40,100,100);
-
+        memberNameLabel.setFont(new Font("맑은고딕",Font.BOLD, 20));
+        memberNameLabel.setBounds(40,10,100,100);
         memberPanel.add(memberNameLabel);
 
         //member ID Label
         JLabel memberIdLabel = new JLabel("ID");
         memberIdLabel.setForeground(new Color(48, 87, 232));
         memberIdLabel.setFont(new Font("맑은고딕",Font.BOLD, 30));
-        memberIdLabel.setBounds(40,200,100,50);
+        memberIdLabel.setBounds(40,120,100,50);
         memberPanel.add(memberIdLabel);
 
         JTextField memberIdTextField = new JTextField(60);
-        memberIdTextField.setBounds(80,210,200,30);
+        memberIdTextField.setBounds(80,130,200,30);
         memberPanel.add(memberIdTextField);
 
         //member PW Label
         JLabel memberPWLabel = new JLabel("PW");
         memberPWLabel.setForeground(new Color(48, 87, 232));
         memberPWLabel.setFont(new Font("맑은고딕",Font.BOLD, 30));
-        memberPWLabel.setBounds(40,400,100,50);
+        memberPWLabel.setBounds(20,200,100,50);
         memberPanel.add(memberPWLabel);
 
         JTextField memberPWTextField = new JTextField(40);
-        memberPWTextField.setBounds(100,410,200,30);
+        memberPWTextField.setBounds(80,210,200,30);
         memberPanel.add(memberPWTextField);
     }
 
@@ -61,38 +69,63 @@ public class LogInWindow extends Template {
         nonMemberPanel.setLayout(null);
         nonMemberPanel.setBackground(Color.white);
         nonMemberPanel.setPreferredSize(new Dimension(500,500));
-        nonMemberPanel.setBounds(710,150,500,500);
+        nonMemberPanel.setBounds(710,90,300,400);
 
         //nonMember Label
         JLabel nonMemberNameLabel = new JLabel("비회원");
 
         nonMemberNameLabel.setForeground(new Color(48, 87, 232));
-        nonMemberNameLabel.setFont(new Font("맑은고딕",Font.BOLD, 30));
-        nonMemberNameLabel.setBounds(40,40,100,100);
+        nonMemberNameLabel.setFont(new Font("맑은고딕",Font.BOLD, 20));
+        nonMemberNameLabel.setBounds(40,10,100,100);
 
         nonMemberPanel.add(nonMemberNameLabel);
 
         //nonmember ID Label
-        JLabel nonMemberIdLabel = new JLabel("ID");
+        JLabel nonMemberIdLabel = new JLabel("이름");
         nonMemberIdLabel.setForeground(new Color(48, 87, 232));
         nonMemberIdLabel.setFont(new Font("맑은고딕",Font.BOLD, 30));
-        nonMemberIdLabel.setBounds(40,200,100,50);
+        nonMemberIdLabel.setBounds(20,120,100,50);
         nonMemberPanel.add(nonMemberIdLabel);
 
         JTextField nonMemberIdTextField = new JTextField(60);
-        nonMemberIdTextField.setBounds(80,210,200,30);
+        nonMemberIdTextField.setBounds(80,130,200,30);
         nonMemberPanel.add(nonMemberIdTextField);
 
         //nonmember PW Label
-        JLabel nonMemberPWLabel = new JLabel("PW");
+        JLabel nonMemberPWLabel = new JLabel("전화번호");
         nonMemberPWLabel.setForeground(new Color(48, 87, 232));
         nonMemberPWLabel.setFont(new Font("맑은고딕",Font.BOLD, 30));
-        nonMemberPWLabel.setBounds(40,400,100,50);
+        nonMemberPWLabel.setBounds(20,200,100,50);
         nonMemberPanel.add(nonMemberPWLabel);
 
         JTextField nonMemberPWTextField = new JTextField(40);
-        nonMemberPWTextField.setBounds(100,410,200,30);
+        nonMemberPWTextField.setBounds(80,210,200,30);
         nonMemberPanel.add(nonMemberPWTextField);
+    }
+
+    void textWindow(){
+        explainLabel = new JLabel("Membership benefits: You can earn 5% of the payment amount.");
+        explainLabel.setForeground(Color.white);
+        explainLabel.setFont(new Font("맑은고딕",Font.BOLD, 30));
+        explainLabel.setBounds(100,500,1000,50);
+    }
+
+    void buttonComponent(){
+        signupButton = new JButton("Sign Up");
+        signupButton.setBounds(100,580,300,40);
+        signupButton.setBackground(Color.WHITE);
+        signupButton.setFont(new Font("맑은고딕", Font.BOLD, 18));
+        signupButton.setForeground(new Color(69,116,203));
+        signupButton.setBorderPainted(false);
+        signupButton.setFocusPainted(false);
+
+        signInButton = new JButton("Sign In");
+        signInButton.setBounds(710,580,300,40);
+        signInButton.setBackground(Color.WHITE);
+        signInButton.setFont(new Font("맑은고딕", Font.BOLD, 18));
+        signInButton.setForeground(new Color(69,116,203));
+        signInButton.setBorderPainted(false);
+        signInButton.setFocusPainted(false);
     }
 
     public static void main(String[]args){

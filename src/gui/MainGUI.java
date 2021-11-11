5176aff5ae32df5900785871f7bfa9aeeb979ee9
INHWA KIM <86849459+radiantbeing99@users.noreply.github.com>
2021-11-12 07:13:25 +0900

    클래스 단순화 및 Window 전환 방식 변경

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,48 +1,26 @@
 package gui;
 
-import javax.swing.*;
-import java.awt.*;
+public class MainGUI {
+    BaseFrame frame = new BaseFrame();
+    // Create Windows
+    static RoomViewWindow roomViewWindow = new RoomViewWindow();
+    static LogInWindow logInWindow = new LogInWindow();
 
-public class MainGUI extends JFrame {
-    JPanel topPanel = new JPanel();
-    JLabel topLabel = new JLabel("BLUEBUTTON");
-    static JPanel centerPanel = new JPanel();
-    Color primaryColor = new Color(69,116,203);
-
-    public void createAndShowGUI() {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(1296, 839)); // contentPane의 size는 1280*800
-        setLocation(300, 100);
-        setTitle("BLUEBUTTON");
-        setInitialComponents();
-        centerPanel.add(new RoomViewWindow());
-        pack();
-        setVisible(true);
+    void run() {
+        frame.createAndShowGUI();
+        BaseFrame.centerPanel.add(roomViewWindow);
     }
 
-    void setInitialComponents() {
-        Container contentPane = getContentPane();
-
-        // About topPanel
-        contentPane.add(topPanel, BorderLayout.PAGE_START);
-        topPanel.setLayout(null);
-        topPanel.setPreferredSize(new Dimension(1280, 80));
-        topPanel.setBackground(Color.WHITE);
-        topPanel.setForeground(Color.WHITE);
-
-        // About topLabel
-        topPanel.add(topLabel, FlowLayout.LEFT);
-        topLabel.setFont(new Font("맑은고딕", Font.BOLD, 30));
-        topLabel.setForeground(primaryColor);
-        topLabel.setBounds(20, 15, 400, 50);
-
-        // About centerPanel
-        contentPane.add(centerPanel, BorderLayout.CENTER);
+    static void changeWindow(Template window) {
+        BaseFrame.centerPanel.removeAll();
+        BaseFrame.centerPanel.revalidate();
+        BaseFrame.centerPanel.repaint();
+        BaseFrame.centerPanel.add(window);
     }
 
     public static void main(String[] args) {
         MainGUI main = new MainGUI();
-        main.createAndShowGUI();
+        main.run();
     }
 
 }

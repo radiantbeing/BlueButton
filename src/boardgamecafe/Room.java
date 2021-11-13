a118f92dd91302d90a2f5de7aadbbfc45cfd3059
chyun7114 <77336660+chyun7114@users.noreply.github.com>
2021-11-13 21:00:04 +0900

    room 정보가 담겨진 class (예약, 사용 중, 이용가능인원 등)

---
+++ src/boardgamecafe/Room.java
@@ -0,0 +1,34 @@
+package boardgamecafe;
+
+import java.util.Scanner;
+
+import mgr.Manageable;
+
+public class Room implements Manageable{
+	//인원 최대 4명, 총 방 10개
+	boolean reserve = false;			// false = 현재 예약 가능 상태, true = 현재 예약완료
+	boolean use = false;				// false = 현재 사용중이 아님(예약 가능), true = 현재 사용중(예약 불가)
+	int person = 0;
+	int roomNumber = 0;
+	@Override
+	public void print() {
+		System.out.printf("[%d번방] ", roomNumber++);
+		if(this.reserve == false && this.use == false)
+			System.out.printf("이용 가능 인원 : %d\t예약가능(미사용중)\n", person);
+		else if(this.reserve == true && this.use == false)
+			System.out.printf("현재 예약 인원 : %d\t예약중\n", person);
+		else
+			System.out.printf("현재 이용 인원 : %d\t현재 사용중\n", person);
+	}
+
+	@Override
+	public void read(Scanner scan) {
+		// TODO Auto-generated method stub	
+	}
+
+	@Override
+	public boolean matches(String wkd) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}

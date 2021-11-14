3727571172bcae4fc3d17994477bdffa0c3d170e
radiantbeing99 <radiantbeing99@gmail.com>
2021-11-14 16:34:45 +0900

    BaseFrame 선언, 생성 위치 변경

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,22 +1,23 @@
 package gui;
 
 public class MainGUI {
+    // Create BaseFrame
+    static BaseFrame bFrame = new BaseFrame();
     // Create Windows
     static RoomViewWindow roomViewWindow = new RoomViewWindow();
     static LogInWindow logInWindow = new LogInWindow();
     static AdminLoginWindow adminLoginWindow = new AdminLoginWindow();
     
     void run() {
-        BaseFrame frame = new BaseFrame();
-        frame.createAndShowGUI();
-        BaseFrame.centerPanel.add(roomViewWindow);
+        bFrame.createAndShowGUI();
+        bFrame.centerPanel.add(roomViewWindow);
     }
 
     static void changeWindow(Template window) {
-        BaseFrame.centerPanel.removeAll();
-        BaseFrame.centerPanel.revalidate();
-        BaseFrame.centerPanel.repaint();
-        BaseFrame.centerPanel.add(window);
+        bFrame.centerPanel.removeAll();
+        bFrame.centerPanel.revalidate();
+        bFrame.centerPanel.repaint();
+        bFrame.centerPanel.add(window);
     }
 
     public static void main(String[] args) {

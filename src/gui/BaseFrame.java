e98408abc10ff1ca652b014bead88cbbfbb65fe0
INHWA KIM <86849459+radiantbeing99@users.noreply.github.com>
2021-11-17 03:34:42 +0900

    프로젝트에 NunumGothic Font 삽입

--- src/gui/BaseFrame.java
+++ src/gui/BaseFrame.java
@@ -2,6 +2,8 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class BaseFrame extends JFrame {
     JPanel topPanel = new JPanel();
@@ -25,16 +27,27 @@ public class BaseFrame extends JFrame {
         contentPane.add(topPanel, BorderLayout.PAGE_START);
         topPanel.setLayout(null);
         topPanel.setPreferredSize(new Dimension(1280, 80));
-        topPanel.setBackground(Color.WHITE);
+        topPanel.setBackground(new Color(41, 42, 45));
         topPanel.setForeground(Color.WHITE);
 
         // About topLabel
         topPanel.add(topLabel, FlowLayout.LEFT);
-        topLabel.setFont(new Font("맑은고딕", Font.BOLD, 30));
-        topLabel.setForeground(new Color(69,116,203));
+        topLabel.setFont(new Font("NanumGothic", Font.BOLD, 30));
+        topLabel.setForeground(new Color(0, 120, 242));
         topLabel.setBounds(20, 15, 400, 50);
 
         // About centerPanel
         contentPane.add(centerPanel, BorderLayout.CENTER);
+        centerPanel.setBackground(new Color(30, 31, 33));
+
+        // Add NunumGothic Font Family
+        GraphicsEnvironment ge =
+                GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT,
+                    new File("Fonts/NanumGothic.ttf")));
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }

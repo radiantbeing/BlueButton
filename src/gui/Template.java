5633a94aa978466c6c96cdb86538e4c6566b5168
INHWA KIM <86849459+radiantbeing99@users.noreply.github.com>
2021-11-17 05:20:56 +0900

    주석 수정

--- src/gui/Template.java
+++ src/gui/Template.java
@@ -7,9 +7,9 @@ import java.awt.*;
     1. ~Window 클래스를 만들고 Template를 상속시킵니다.
     2. addComponents()을 오버라이딩하여 구현합니다.
     3. MainGUI의 "// Create Windows" 주석 아랫 부분에 static으로 인스턴스를 만듭니다.
-        * MainGUI 클래스의 4~6줄 참고
+        * MainGUI 클래스의 9~13줄 참고
     3. 리스너에서 Window를 전환하는 명령을 작성합니다.
-        * RoomViewWindow 클래스의 57~62줄 참고
+        * RoomViewWindow 클래스의 48-53줄 참고
 
     *** Template를 상속한 클래스는 1280*720 크기의 JPanel입니다.
     *** 사용 예시를 보고 싶다면 RoomViewWindow를 참고하세요.

92004458cf748ed3c09833fcaaa27cdfec6acb9f
chyun7114 <77336660+chyun7114@users.noreply.github.com>
2021-11-29 22:35:41 +0900

    중복된 코드 수정

--- src/gui/TimeSelectWindow.java
+++ src/gui/TimeSelectWindow.java
@@ -77,8 +77,7 @@ public class TimeSelectWindow extends Template {
                 public void actionPerformed(ActionEvent e) {
                 	NonMember m = (NonMember) LogInWindow.getNowLoginMember();
                     hour = buttonStr[tmp];
-                    price = Integer.parseInt(timeStr[tmp].substring(0, timeStr[tmp].length() - 1));
-                    m.addTime(Integer.parseInt(hour.substring(0, 1)) * 3600);
+                    price = Integer.parseInt(timeStr[tmp].substring(0, timeStr[tmp].length() - 1));            
                     JOptionPane.showMessageDialog(null, hour + " " + price + "원으로 선택합니다.");
                 }
             });

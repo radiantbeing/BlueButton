335a460fcd63e796c3c48923bd38d280720e45dd
MS <akdcb0219@naver.com>
2021-11-30 00:32:15 +0900

    MypageWindow 수정

--- src/gui/MypageWindow.java
+++ src/gui/MypageWindow.java
@@ -1,5 +1,6 @@
 package gui;
 
+import boardgamecafe.NonMember;
 import gui.template.BasicButton;
 import gui.template.BasicLabel;
 import gui.template.BasicPanel;
@@ -7,10 +8,13 @@ import gui.template.Template;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MypageWindow extends Template {
     BasicPanel MypagePanel;
     BasicLabel remaintimeLabel, gameLabel, totalpriceLabel;
+    NonMember m = (NonMember) LogInWindow.getNowLoginMember();
     @Override
     public void addComponents() {
         MypagePanel = new BasicPanel();
@@ -46,21 +50,45 @@ public class MypageWindow extends Template {
         BasicButton timeaddButton = new BasicButton("시간 추가");
         timeaddButton.setBounds(25, 150, 150, 40);
         timeaddButton.setFontAttribute(20);
+        timeaddButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainGUI.changeWindow(MainGUI.timeSelectWindow);
+            }
+        });
 
         // -------------- 게임 변경 버튼
         BasicButton gameButton = new BasicButton("게임 변경");
         gameButton.setBounds(10, 200, 150, 40);
         gameButton.setFontAttribute(20);
+        gameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainGUI.changeWindow(MainGUI.gameSelectWindow);
+            }
+        });
 
         // -------------- 간식 주문 버튼
         BasicButton snackaddButton = new BasicButton("간식 주문");
         snackaddButton.setBounds(10, 250, 150, 40);
         snackaddButton.setFontAttribute(20);
+        snackaddButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainGUI.changeWindow(MainGUI.snackOrderWindow);
+            }
+        });
 
         // -------------- 결제 완료 버튼
         BasicButton paymentButton = new BasicButton("결제 완료");
         paymentButton.setBounds(1000, 600, 150, 40);
         paymentButton.setFontAttribute(20);
+        paymentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainGUI.changeWindow(MainGUI.roomViewWindow);
+            }
+        });
 
 
         mypagePanel.add(remaintimeLabel);

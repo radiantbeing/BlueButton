9d9de91f0bd943e73a21f442ce13c33a4f811332
radiantbeing99 <radiantbeing99@gmail.com>
2021-12-01 17:20:06 +0900

    사용 중인 방 선택 불가능 하도록 변경

--- src/gui/RoomSelectWindow.java
+++ src/gui/RoomSelectWindow.java
@@ -24,6 +24,7 @@ public class RoomSelectWindow extends Template {
     static BasicLabel[] roomPanelTexts = new BasicLabel[10];
     Room selectedRoom;
     ArrayList<JPanel> roomPanelArrayList;
+
     public void addComponents() {
         setLayout(null);
         JPanel roomViewPanel = new JPanel();
@@ -47,11 +48,11 @@ public class RoomSelectWindow extends Template {
             JPanel roomPanel = new JPanel(null);
             roomPanel.setPreferredSize(new Dimension(200, 250));
             // 예약된 방은 빨강, 예약 가능 방은 초록
-            boolean roomReserved =  RoomViewWindow.checkRoomReserved(i+1);
+            boolean roomReserved = RoomViewWindow.checkRoomReserved(i + 1);
             if (roomReserved) {
-                roomPanel.setBackground(new Color(115,52,54));
+                roomPanel.setBackground(new Color(115, 52, 54));
             } else {
-                roomPanel.setBackground(new Color(62,111,74));
+                roomPanel.setBackground(new Color(62, 111, 74));
             }
             roomPanel.setBorder(new LineBorder(new Color(30, 31, 33), 1));
             roomPanelTexts[i] = new BasicLabel("" + (i + 1));
@@ -66,23 +67,24 @@ public class RoomSelectWindow extends Template {
             roomViewPanel.add(rPanel);
         }
 
-        int i=0;
-        for(JPanel rPanel : roomPanelArrayList){
+        int i = 0;
+        for (JPanel rPanel : roomPanelArrayList) {
             int finalI = i;
             rPanel.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
-                    clearRoom();
-                    if (e.getClickCount() % 2 != 0) { //클릭시
-                        roomNum = Integer.parseInt(roomPanelTexts[finalI].getText());
-                        rPanel.setBorder(new LineBorder(new Color(0, 120, 242), 3));
-                        nextButton.setBackground(new Color(0, 120, 242));
-                        nextButton.setEnabled(true);
-                    }
-                    else {//재클릭시
-                        rPanel.setBorder(new LineBorder(new Color(30, 31, 33)));
-                        nextButton.setBackground(new Color(121, 117, 117));
-                        nextButton.setEnabled(false);
+                    if (isNumeric((roomPanelTexts[finalI].getText()))) {
+                        clearRoom();
+                        if (e.getClickCount() % 2 != 0) { //클릭시
+                            roomNum = Integer.parseInt(roomPanelTexts[finalI].getText());
+                            rPanel.setBorder(new LineBorder(new Color(0, 120, 242), 3));
+                            nextButton.setBackground(new Color(0, 120, 242));
+                            nextButton.setEnabled(true);
+                        } else {//재클릭시
+                            rPanel.setBorder(new LineBorder(new Color(30, 31, 33)));
+                            nextButton.setBackground(new Color(121, 117, 117));
+                            nextButton.setEnabled(false);
+                        }
                     }
                 }
             });
@@ -90,8 +92,8 @@ public class RoomSelectWindow extends Template {
         }
     }
 
-    void clearRoom(){
-        for(JPanel rPanel : roomPanelArrayList){
+    void clearRoom() {
+        for (JPanel rPanel : roomPanelArrayList) {
             rPanel.setBorder(new LineBorder(new Color(30, 31, 33)));
         }
     }
@@ -105,17 +107,17 @@ public class RoomSelectWindow extends Template {
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	NonMember m = LogInWindow.getNowLoginMember();
+                NonMember m = LogInWindow.getNowLoginMember();
 
-                selectedRoom = (Room) BoardGameCafe.roomMgr.getList().get(roomNum-1);
-                if(selectedRoom.use) {
-                	JOptionPane.showMessageDialog(null, "이미 사용중인 방입니다");
-                	return;
+                selectedRoom = (Room) BoardGameCafe.roomMgr.getList().get(roomNum - 1);
+                if (selectedRoom.use) {
+                    JOptionPane.showMessageDialog(null, "이미 사용중인 방입니다");
+                    return;
                 }
 
 
                 //선택효과 초기화
-                for(JPanel j: roomPanelArrayList){
+                for (JPanel j : roomPanelArrayList) {
                     j.setBorder(new LineBorder(new Color(30, 31, 33), 1));
                 }
                 //버튼초기화
@@ -144,23 +146,32 @@ public class RoomSelectWindow extends Template {
     void changeRoomInfo(int roomNum, boolean flag) {
         JPanel jPanel = roomPanelArrayList.get(roomNum - 1);
         if (flag) {//방이 이용중일때
-            jPanel.setBackground(new Color(115,52,54));
+            jPanel.setBackground(new Color(115, 52, 54));
             return;
         }
         //이용시간이 끝나거나 이용중이 아니면
-        jPanel.setBackground(new Color(62,111,74));
+        jPanel.setBackground(new Color(62, 111, 74));
     }
 
-    static void changeRoomText(int roomNum,String name, String gameName){
-        if(LogInWindow.getNowLoginMember().timerFlag){
-            roomPanelTexts[roomNum-1].setFontAttribute(15);
-            roomPanelTexts[roomNum-1].setBounds(10, 75, 300, 100);
-            roomPanelTexts[roomNum-1].setText("<HTML><body><center>사용자: " + name +
+    static void changeRoomText(int roomNum, String name, String gameName) {
+        if (LogInWindow.getNowLoginMember().timerFlag) {
+            roomPanelTexts[roomNum - 1].setFontAttribute(15);
+            roomPanelTexts[roomNum - 1].setBounds(10, 75, 300, 100);
+            roomPanelTexts[roomNum - 1].setText("<HTML><body><center>사용자: " + name +
                     "<br>사용중인게임: " + gameName + "</center></body></HTML>");//레이아웃에 맞게 수정예정
             return;
         }
-        roomPanelTexts[roomNum-1].setFontAttribute(18);
-        roomPanelTexts[roomNum-1].setBounds(95, 75, 100, 100);
-        roomPanelTexts[roomNum-1].setText(roomNum+"");
+        roomPanelTexts[roomNum - 1].setFontAttribute(18);
+        roomPanelTexts[roomNum - 1].setBounds(95, 75, 100, 100);
+        roomPanelTexts[roomNum - 1].setText(roomNum + "");
+    }
+
+    boolean isNumeric(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
